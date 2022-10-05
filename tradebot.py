5a35eeb12790529354aefcb1f18bdcd0748e4ca2
Sidio01 <sidio1992@gmail.com>
2022-10-05 23:47:40 +0300

    added func to navigate on page

--- tradebot.py
+++ tradebot.py
@@ -1,16 +1,7 @@
-from copy import deepcopy
 import os
 from random import choice
 import time
-from typing import List
 
-from icecream import ic
-import numpy as np
-import pandas as pd
-import openpyxl
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.preprocessing import normalize
-from tqdm import tqdm
 from dotenv import load_dotenv
 
 from selenium import webdriver
@@ -19,6 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from selenium.common.exceptions import ElementClickInterceptedException
 
 from user_agents import ua
 
@@ -39,12 +31,35 @@ class SeleniumDriver():
         return self.driver
 
 
+def click_button(driver, xpath):
+    while True:
+        try:
+            btn = WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            btn.click()
+            break
+        except ElementClickInterceptedException:
+            continue
+
+
+def go_back(driver):
+    click_button(driver, '//button[@class="ut-navigation-button-control"]')
+
+
+def go_next_page(driver):
+    click_button(driver, '//button[contains(@class, "next")]')
+
+
+def determine_min_buyout_price(driver):
+    buyout_price_list = driver.find_elements(By.XPATH, '//div[@class="auctionValue"]//span[contains(@class, "value")]')
+    buyout_price_list = [int(price.text.replace(" ", "")) for price in buyout_price_list[1::2]]
+    print(min(buyout_price_list))
+
+
 def create_driver(): 
     url = "https://www.ea.com/ru-ru/fifa/ultimate-team/web-app/"
     driver = SeleniumDriver().get_driver()
 
     driver.get(url)
-
 
     WebDriverWait(driver, timeout=30).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@class, "call-to-action")]')))
     # btn = driver.find_element(By.XPATH, '//button[contains(@class, "call-to-action")]')
@@ -64,9 +79,15 @@ def login(driver):
     print("You have 180 seconds to enter the code.")
 
 
-def enter_transfers(driver):
-    transfer_btn = WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "icon-transfer")]')))
-    transfer_btn.click()
+def enter_transfer_market(driver):
+    click_button(driver, '//button[contains(@class, "icon-transfer")]')
+    click_button(driver, '//div[contains(@class, "ut-tile-transfer-market")]')
+
+
+def search_player(driver):
+    driver.find_element(By.XPATH, '//input[@class="ut-text-input-control"]').send_keys(os.environ.get("PLAYER_TO_SEARCH"))
+    click_button(driver, '//ul[contains(@class, "playerResultsList")]/button')
+    click_button(driver, '//button[contains(@class, "call-to-action")]')
 
 
 if __name__ == "__main__":
@@ -74,5 +95,10 @@ if __name__ == "__main__":
     driver = create_driver()
     login(driver)
     WebDriverWait(driver, timeout=180).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@class, "icon-transfer")]')))
-    enter_transfers(driver)
-    time.sleep(300)
+    enter_transfer_market(driver)
+    search_player(driver)
+    determine_min_buyout_price(driver)
+    time.sleep(5)
+    go_back(driver)
+    time.sleep(600)
+    driver.quit()
