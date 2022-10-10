67791a19cab272b83581350a0521fbb23d71e6ff
Sidio01 <sidio1992@gmail.com>
2022-10-11 00:44:00 +0300

    update listing trade pile, added relist, clear sold

---
+++ config.py
@@ -0,0 +1,1 @@
+PLAYERS_TO_LIST_XPATH = '//li[contains(@class, "listFUTItem") and not(contains(@class, "has-auction-data"))]//div[@class="name"]'
--- tradebot.py
+++ tradebot.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import os
 from random import choice
-from statistics import median, mode
+from statistics import median, mode, mean
 import time
 
 from dotenv import load_dotenv
@@ -13,8 +13,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementClickInterceptedException, StaleElementReferenceException, WebDriverException
 
+from config import PLAYERS_TO_LIST_XPATH
 from user_agents import ua
 
 class SeleniumDriver():
@@ -35,12 +36,15 @@ class SeleniumDriver():
 
 
 def click_button(driver, xpath):
+    # TODO: update for created buttons
     while True:
         try:
             btn = WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, xpath)))
             btn.click()
             break
         except ElementClickInterceptedException:
+            continue
+        except StaleElementReferenceException:
             continue
 
 
@@ -60,32 +64,38 @@ def go_main_page(driver):
     click_button(driver, '//button[contains(@class, "icon-home")]')
 
 
-def determine_min_buyout_price(driver, section=None): #, player_name=None):
+def determine_min_buyout_price(driver, section=None, pages_to_scan=10): #, player_name=None):
     # if player_name is None:
     #     compare_player_price(driver)
     # else:
     #     search_player(driver, player_name)
+    # statistics.StatisticsError: no median for empty data
     buyout_price_list_total = []
-    while True:
+    # while True:
+    for _ in range(pages_to_scan):
+        time.sleep(0.75)
+        # FIXME: sometimes break at start
         if driver.find_element(By.XPATH, '//button[contains(@class, "next")]').get_attribute("style") == "display: none;":
             break
-        time.sleep(2)
-        # TODO:
-        # selenium.common.exceptions.StaleElementReferenceException: Message: The element reference of <span class="currency-coins value"> is stale; 
-        # either the element is no longer attached to the DOM, it is not in the current frame context, or the document has been refreshed
-        if section is None:
-            buyout_price_list = driver.find_elements(By.XPATH, '//div[@class="auctionValue"]//span[contains(@class, "value")]')
-        else:
-            buyout_price_list = driver.find_elements(By.XPATH, f'{section}//div[@class="auctionValue"]//span[contains(@class, "value")]')
-        buyout_price_list = [int(price.text.replace(" ", "").replace(",", "")) for price in buyout_price_list[1::2] if price.text.replace(" ", "").replace(",", "") != ""]
+        while True:
+            try:
+                if section is None:
+                    buyout_price_list = driver.find_elements(By.XPATH, '//div[@class="auctionValue"]//span[contains(@class, "value")]')
+                else:
+                    buyout_price_list = driver.find_elements(By.XPATH, f'{section}//div[@class="auctionValue"]//span[contains(@class, "value")]')
+                buyout_price_list = [int(price.text.replace(" ", "").replace(",", "")) for price in buyout_price_list[1::2] if price.text.replace(" ", "").replace(",", "") != ""]
+                break
+            except StaleElementReferenceException:
+                continue
         buyout_price_list_total.extend(buyout_price_list)
         go_next_page(driver)
     # TODO: choose correct price
-    print("dict -", Counter(buyout_price_list_total))
-    print("median -", median(buyout_price_list_total))
-    print("mode -", mode(buyout_price_list_total))
+    # print("dict -", Counter(buyout_price_list_total))
+    # print("median -", median(buyout_price_list_total))
+    # print("mode -", mode(buyout_price_list_total))
     # go_main_page(driver)
-    return median(buyout_price_list_total) * 0.95
+    return mean(sorted(buyout_price_list_total)[:5])
+    # return median(buyout_price_list_total) * 0.925
 
 
 def list_on_transfer_market(driver, price):
@@ -96,7 +106,6 @@ def list_on_transfer_market(driver, price):
     buyout = driver.find_element(By.XPATH, '(//input[contains(@class, "ut-number-input-control")])[2]')
     buyout.click()
     buyout.send_keys(price)
-    time.sleep(60)
     click_button(driver, '(//button[contains(@class, "call-to-action")])[last()]')
 
 
@@ -104,39 +113,71 @@ def locate_player_in_transfer_pile(driver):
     pass
 
 
+def relist(driver):
+    click_button(driver, '//button[text()="Re-list All" and not(@style="display: none;")]')
+    # TODO: click "yes"
+
+
+def clear_sold(driver):
+    click_button(driver, '//button[text()="Clear Sold" and not(@style="display: none;")]')
+
+
 def list_players_in_transfer_pile(driver):
     enter_transfer_list(driver)
     time.sleep(2.5)
-    players_to_sell = driver.find_elements(By.XPATH, '//li[contains(@class, "listFUTItem")]//div[@class="name"]')
+    # TODO: dict with players price to avoid scanning duplicates
+    scanned_players = dict()
+    # TODO: remove all sold
+    if len(driver.find_elements(By.XPATH, '//li[contains(@class, "won")]//div[@class="name"]')) > 1:
+        relist(driver)
+    # TODO: relist
+    if len(driver.find_elements(By.XPATH, '//li[contains(@class, "expired")]//div[@class="name"]')) > 1:
+        relist(driver)
+    
+    players_to_sell = driver.find_elements(By.XPATH, PLAYERS_TO_LIST_XPATH)
+    players_names = [player_name.text for player_name in players_to_sell]
     # for player in players_to_sell:
-    for idx, player in enumerate(players_to_sell[:2]):
-        if idx == 0:
-            pass
+    # FIXME: error if one player will be sold during listing
+    print(len(players_to_sell))
+    for idx in range(len(players_to_sell)):
+        current_player_name = players_names[idx]
+        print(current_player_name)
+        print(scanned_players)
+        if current_player_name in scanned_players:
+            time.sleep(1.5)
+            list_on_transfer_market(driver, current_player_price)
         else:
-            while True:
-                try:
-                    player.click()
-                    break
-                except ElementClickInterceptedException:
-                    continue
-            
-        click_button(driver, '//span[@class="btn-text" and text()="Compare Price"]/parent::button[@class]') # FIXME: can't locate correctly without hover it manually 
-        # player_price = determine_min_buyout_price(driver, section='//section[contains(@class, "ui-layout-right")]')
-        player_price = 1615.0
-        print(player_price)
-        go_back_last(driver)
-        # enter_transfer_list(driver)
-        # click_button(driver, f'//div[@class="name" and text()="{player}"]')
-        list_on_transfer_market(driver, player_price)
+            click_button(driver, PLAYERS_TO_LIST_XPATH)
+
+            action = webdriver.ActionChains(driver)
+            element = driver.find_element(By.XPATH, '//div[@class="ut-quick-list-panel-view"]')
+            action.move_to_element(element)
+            # for i in range(50):
+            #     action.move_by_offset(0, i)
+            action.move_by_offset(0, 175)
+            action.perform()
+            click_button(driver, '//span[@class="btn-text" and text()="Compare Price"]/parent::button[@class]') # FIXME: can't locate correctly without hover it manually 
+
+            # TODO: too many pages to analyze
+            current_player_price = determine_min_buyout_price(driver, section='//section[contains(@class, "ui-layout-right")]')
+            print(current_player_price)
+            click_button(driver, PLAYERS_TO_LIST_XPATH)
+            # enter_transfer_list(driver)
+            # click_button(driver, f'//div[@class="name" and text()="{player}"]')
+            scanned_players[current_player_name] = current_player_price
+            list_on_transfer_market(driver, current_player_price)
 
 
 def create_driver(): 
     url = "https://www.ea.com/en-en/fifa/ultimate-team/web-app/"
     driver = SeleniumDriver().get_driver()
     
-    # TODO:
-    # selenium.common.exceptions.WebDriverException: Message: Reached error page:
-    driver.get(url)
+    while True:
+        try:
+            driver.get(url)
+            break
+        except WebDriverException:
+            continue
 
     WebDriverWait(driver, timeout=30).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@class, "call-to-action")]')))
     # btn = driver.find_element(By.XPATH, '//button[contains(@class, "call-to-action")]')
@@ -153,6 +194,7 @@ def login(driver):
     send_code_btn = WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//a[@id="btnSendCode"]')))
     send_code_btn.click()
     print("You have 180 seconds to enter the code.")
+    # TODO: enter code by input
 
 
 def enter_transfer_market(driver):
